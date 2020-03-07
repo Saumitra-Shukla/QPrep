f8ebaa523517d21a3c44a618c4305339cdefa31e
CrioUser <crio-user@criodo.com>
2020-03-07 11:21:40 +0000

    [LocalTest]:[LetterCombinationsOfPhoneNumber]:[java]

--- LetterCombinationsOfPhoneNumber/LetterCombinationsOfPhoneNumber.java
+++ LetterCombinationsOfPhoneNumber/LetterCombinationsOfPhoneNumber.java
@@ -4,10 +4,51 @@ import java.lang.*;
 
 public class LetterCombinationsOfPhoneNumber {
 
+
+    public int limit(int d) {
+        int ans = (d * 2) + (d - 3);
+        if (d > 6) {
+            ans++;
+        }
+        if (d > 8) {
+            ans++;
+        }
+        if (d < 2) {
+            ans = 0;
+        } 
+        return ans;
+    }
+
+    public List<String> letters(int d) {
+        int b=0,e=0;
+        e = limit(d);
+        char end = (char) (e + 97);
+        b = limit(d-1);
+        char begin = (char) (b + 97);
+        List<String> pat = new ArrayList<String>(); 
+        for(char i = begin; i <= end; ++i) {
+            pat.add(Character.toString(i));
+        }
+        return pat;
+    }
+
     // Implement your solution by completing the below function
     public List<String> letterCombinations(String digits) {
+        int d = Integer.parseInt(digits);
+        String d1 = Character.toString(digits.charAt(0));
+        String d2 = Character.toString(digits.charAt(1));
+        List<String> num1 = letters(Integer.parseInt(d1));
+        List<String> num2 = letters(Integer.parseInt(d2));
+        List<String> pat = new ArrayList<String>();
+        for(int i = 0; i < num1.size(); i++) {
+            for (int j=0; j < num2.size(); j++) {
+                String ans = num1.get(i) + num2.get(j);
+                pat.add(ans);
+            }
+        }
 
-        return new List<String>();
+        
+        return pat;
     }
 
     public static void main(String[] args) throws IOException {
