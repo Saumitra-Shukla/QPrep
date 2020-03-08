0fea96049ba3c08fd28466a2e35097cf1de85419
CrioUser <crio-user@criodo.com>
2020-03-08 03:45:24 +0000

    [LocalTest]:[MergeIntervals]:[java]

--- MergeIntervals/MergeIntervals.java
+++ MergeIntervals/MergeIntervals.java
@@ -3,9 +3,62 @@ import java.util.*;
 
 class MergeIntervals {
     // Implement your solution by completing the below function
-    public int[][] merge(int[][] intervals) {
 
-        return null;
+    class Interval {
+        int begin;
+        int end;
+        Intervals(int b, int e) {
+            begin = b;
+            end = e;
+        }
+    }
+    public List<Interval> sort(List<Interval> interv) {
+        
+        for(int i = 0; i < interv.size-1; i++) {
+            for(int j = 0; j < interv.size-1-i; j++) {
+                if(interv.get(j).begin > interv.get(j + 1).begin) {
+                    Interval swp = interv.get(j);
+                    interv.set(j, interv.get(j + 1));
+                    interv.set(j + 1, swp);
+                }
+            }
+        }
+        
+        return interv;
+    }
+
+    public List<Interval> merger(List<Interval> interv) {
+        
+        for(int i = 1; i < interv.size(); i++){
+            if(interv.get(i-1).end > interv.get(i).begin) {
+                int begin = Math.min(interv.get(i-1).begin, interv.get(i).begin);
+                int end = Math.max(interv.get(i-1).end, interv.get(i).end);
+                Intervals temp = new Interval(begin, end);
+                interv.set(i-1, temp);
+                interv.remove(i);
+            }
+        }
+        return interv;
+        
+    }
+
+    public int[][] merge(int[][] intervals) {
+        
+        List<Interval> interv = new ArrayList<Interval>();
+
+        for(int i = 0; i < intervals.length; i++ ) {
+            interv.add(new Interval(intervals[i][0], intervals[i][1]));
+        }
+        interv = merger(sort(interv));
+        
+        int [][] ans = new int[inter.size()][2];
+        for(int i1 = 0; i1 < interv.size(); i1++) {
+            ans[i][0] = interv.get(i).begin;
+            ans[i][1] = inter.get(i).end;
+        }
+
+
+        return ans;
     }
 
     public static void main(String[] args) {
