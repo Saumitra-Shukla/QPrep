70e7d713054609d51f30f69bdd6b551c1b60080e
CrioUser <crio-user@criodo.com>
2020-03-08 15:19:07 +0000

    [LocalTest]:[MergeIntervals]:[java]

--- MergeIntervals/MergeIntervals.java
+++ MergeIntervals/MergeIntervals.java
@@ -2,84 +2,59 @@ import java.io.*;
 import java.util.*;
 
 class MergeIntervals {
-    // Implement your solution by completing the below function
 
-    class Interval {
-        int begin;
-        int end;
-        Interval(int b, int e) {
-            begin = b;
-            end = e;
-        }
-        public void print()
-        {
-            System.out.print(begin+","+end +"   ");
-        }
+    final int[][] resize(int[][] matrix, int w, int h) {
+        int[][] temp = new int[h][w];
+        h = Math.min(h, matrix.length);
+        w = Math.min(w, matrix[0].length);
+        for (int i = 0; i < h; i++)
+            System.arraycopy(matrix[i], 0, temp[i], 0, w);
+        return temp;
     }
-    
-    public void printer(List<Interval> interv)
-    {
-        for(Interval i : interv)
-        {
-            i.print();
-        }
-    }
-    public List<Interval> sort(List<Interval> interv) {
-        
-        for(int i = 0; i < interv.size()-1; i++) {
-            for(int j = 0; j < interv.size()-1-i; j++) {
-                if(interv.get(j).begin > interv.get(j + 1).begin) {
-                    Interval swp = interv.get(j);
-                    interv.set(j, interv.get(j + 1));
-                    interv.set(j + 1, swp);
-                }
+
+    final Comparator<int[]> newcomparator=new Comparator<int[]>(){
+    @Override
+    public int compare(int[] a1, int[] a2){
+        if(a1[0]==a2[0]){
+            if(a1[1]>a2[1]){
+                return 1;
             }
+            return -1;
         }
-        
-        return interv;
+        if(a1[0]>a2[0]){
+            return 1;
+        }
+            return -1;
     }
-
-    public List<Interval> merger(List<Interval> interv, int index) {
-        int i = index;
-        if(interv.size() == index) {
-            //System.out.println("\n last   ");
-            
-            return interv;
-            
-        }
-        if(interv.get(i - 1).end >= interv.get(i).begin) {
-            int begin = Math.min(interv.get(i-1).begin, interv.get(i).begin);
-            int end = Math.max(interv.get(i-1).end, interv.get(i).end);
-            Interval temp = new Interval(begin, end);
-            interv.set(i-1, temp);
-            interv.remove(i);
-            //System.out.print("\n i    ");
-            //printer(interv); 
-            return merger(interv, i);
-            
-        }
-        //System.out.print("\n i+1   ");
-        //printer(interv);
-        return merger(interv, i+1);
-    }
+};
 
     public int[][] merge(int[][] intervals) {
-        
-        List<Interval> interv = new ArrayList<Interval>();
 
-        for(int i = 0; i < intervals.length; i++ ) {
-            interv.add(new Interval(intervals[i][0], intervals[i][1]));
+        int r = intervals.length;
+        int c = 2;
+        if(r<=0){
+            return null;
         }
-        interv = merger(sort(interv), 1);
-        
-        int [][] ans = new int[interv.size()][2];
-        for(int i1 = 0; i1 < interv.size(); i1++) {
-            ans[i1][0] = interv.get(i1).begin;
-            ans[i1][1] = interv.get(i1).end;
+        Arrays.sort(intervals, newcomparator);
+        int[][] ans = new int[r][c];
+        int j = 0;
+        ans[0][0] = intervals[0][0];
+        ans[0][1] = intervals[0][1];
+        int maxi = intervals[0][1];
+        for (int i = 1; i < r; i++) {
+            if (intervals[i][0] <= maxi) {
+                maxi = Math.max(maxi,intervals[i][1]);
+                ans[j][1] = maxi; 
+            } else {
+                j++;
+                ans[j][0] = intervals[i][0];
+                ans[j][1] = intervals[i][1];
+                maxi = ans[j][1];
+            }
         }
 
-
-        return ans;
+        int[][] res = resize(ans,2,j+1);
+        return res;
     }
 
     public static void main(String[] args) {
@@ -93,8 +68,9 @@ class MergeIntervals {
         scanner.close();
 
         int[][] results = new MergeIntervals().merge(nums);
-        //System.out.println("*************************");
-        for (int i = 0; i < results.length; ++i)
+
+        for (int i = 0; i < results.length; ++i) {
             System.out.println(String.valueOf(results[i][0]) + " " + String.valueOf(results[i][1]));
+        }
     }
 }
