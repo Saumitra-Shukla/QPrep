1fe32a5462d7f84e11bdd5b967daa69655b35bf5
CrioUser <crio-user@criodo.com>
2020-03-09 22:34:37 +0000

    [LocalTest]:[SpiralMatrix]:[java]

--- SpiralMatrix/SpiralMatrix.java
+++ SpiralMatrix/SpiralMatrix.java
@@ -44,22 +44,33 @@ class SpiralMatrix {
         List<Integer> lst = new ArrayList<Integer>();
         int m = matrix.length;
         int n = matrix[0].length;
-        try {
-
-            for(int i = 0; i < Math.min(m,n) - 1; i++) {
-                
-                lst = add(lst, goright(matrix, i, m, n));
-                lst = add(lst, godown(matrix, i, m, n));
-                if(m != n && i == Math.min(m,n) - 2){
-                    break;
-                }
-                lst = add(lst, goleft(matrix, i, m, n));
-                lst = add(lst, goup(matrix, i, m, n));
-            }
-        }
-        catch(Exception e)
-        {
-            return lst;
+        
+        int i, k = 0, l = 0; 
+ 
+        while (k < m && l < n) { 
+            for (i = l; i < n; ++i) { 
+                lst.add(matrix[k][i]); 
+            } 
+            k++; 
+  
+            for (i = k; i < m; ++i) { 
+                lst.add(matrix[i][n - 1]); 
+            } 
+            n--; 
+  
+            if (k < m) { 
+                for (i = n - 1; i >= l; --i) { 
+                    lst.add(matrix[m - 1][i]); 
+                } 
+                m--; 
+            } 
+  
+            if (l < n) { 
+                for (i = m - 1; i >= k; --i) { 
+                    lst.add(matrix[i][l]); 
+                } 
+                l++; 
+            } 
         }
         
         return lst;
