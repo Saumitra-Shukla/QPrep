a47710bfcaae141a0d2586d53767b9123a47227c
CrioUser <crio-user@criodo.com>
2020-03-15 22:07:17 +0000

    [LocalTest]:[TwoSum]:[python]

--- TwoSum/TwoSum.py
+++ TwoSum/TwoSum.py
@@ -1,9 +1,22 @@
 from crio.python.io import PrintMatrix
 
 # Implement your solution by completing the below function
-def twoSum(nums, target):
-    v = [0, 0]
+def check(nums,i,j,target):
+    if i==j:
+        return [0,0]
+    val=nums[i]+nums[j]
+    if val == target:
+        return [i,j]
+    if val < target:
+        return check(nums,i+1,j,target)
+    if val > target:
+        return check(nums,i,j-1,target)
 
+
+
+def twoSum(nums, target):
+
+    v = check(nums,0,len(nums)-1,target)
     return v
 
 if __name__ == '__main__':
