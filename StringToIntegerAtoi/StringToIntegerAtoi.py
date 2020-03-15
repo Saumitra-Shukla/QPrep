5c75ce95caaee030abe5a0e3e119c57bfdcb0eb7
CrioUser <crio-user@criodo.com>
2020-03-15 21:46:38 +0000

    [LocalTest]:[StringToIntegerAtoi]:[python]

--- StringToIntegerAtoi/StringToIntegerAtoi.py
+++ StringToIntegerAtoi/StringToIntegerAtoi.py
@@ -1,5 +1,23 @@
 # Implement your solution by completing the below function
 def myAtoi(s):
+    
+    new=''
+    for letter in s:
+    	if (letter.isalpha()):
+    		continue
+    	new+=letter
+    s=new
+    #print(s)
+    		
+    sd=s.strip().strip('-')
+    if len(sd) > 10 or sd == '':
+        return 0
+    if len(sd) == 10:
+        if int(s) > 2**31:
+            return 2**31
+        if int(s) < (-1*(2**31)):
+            return -2**31
+
     x = int(s)
 
     return x
