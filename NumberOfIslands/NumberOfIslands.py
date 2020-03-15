d10685c21850583219d0a01ddc829ccda66fb140
CrioUser <crio-user@criodo.com>
2020-03-15 02:47:23 +0000

    [LocalTest]:[NumberOfIslands]:[python]

--- NumberOfIslands/NumberOfIslands.py
+++ NumberOfIslands/NumberOfIslands.py
@@ -4,7 +4,7 @@
 def dfs(grid, i, j):
     if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
         return
-    grid[i][j] = '2'
+    grid[i][j] = '#'
     dfs(grid, i+1, j)
     dfs(grid, i-1, j)
     dfs(grid, i, j+1)
@@ -22,7 +22,7 @@ def numIslands(grid):
             if grid[i][j] == '1':
                 dfs(grid, i, j)
                 count += 1
-            elif grid[i][j] == '-':
+            elif grid[i][j] != '0' or grid[i][j] != '1' or grid[i][j] != '#' :
                 return 0
     return count
 
@@ -30,7 +30,6 @@ if __name__ == '__main__':
     row = input().split()
     n = int(row[0])
     m = int(row[1])
-
     grid = []
     for i in range(n):
         r = input()
@@ -40,6 +39,4 @@ if __name__ == '__main__':
         grid.append(temp)
 
     result = numIslands(grid)
-    if n<0 or m<0:
-        result=0
     print(result)
