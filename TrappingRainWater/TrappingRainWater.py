99a82608ed2b215c98605b485617d9a43e6ab34f
CrioUser <crio-user@criodo.com>
2020-03-16 01:46:47 +0000

    [LocalTest]:[TrappingRainWater]:[python]

--- TrappingRainWater/TrappingRainWater.py
+++ TrappingRainWater/TrappingRainWater.py
@@ -1,6 +1,34 @@
 # Implement your solution by completing the below function
+def travArr(heights):
+    left=[]
+    large=0
+    for h in heights:
+        left.append(large)
+        if h>large:
+            large=h
+    return left
+
+
+
+
 def trap(heights):
-    ans = 0
+    
+    heights.reverse()
+    right=travArr(heights)
+    right.reverse()
+    
+    heights.reverse()
+    left=travArr(heights)
+
+    ans=0
+    for i,h in enumerate(heights):
+        #print('left = ',left[i],'right = ',right[i],'min = ',min(left[i],right[i]),'h =',h,end='')
+        water=min(left[i],right[i])-h
+        #print('water = ',water)
+        if water < 0:
+            water = 0
+        ans+=water
+    #ans = 0
 
     return ans
 
@@ -10,6 +38,7 @@ if __name__ == '__main__':
     if (n != 0):
         heights = input().split()
         heights = [int(i) for i in heights]
+    #print(heights)
     result = trap(heights)
     print(result)
     
